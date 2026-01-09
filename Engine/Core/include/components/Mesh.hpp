b31fa6bdd42890b5526123db922c0ca8f1cf5b65
DEVU <64399110+oDEVU@users.noreply.github.com>
2026-01-09 15:13:34 +0100

    d
    d

--- Engine/Core/include/components/Mesh.hpp
+++ Engine/Core/include/components/Mesh.hpp
@@ -26,30 +26,47 @@ namespace vex {
         glm::vec3 normal;
         /// @brief UV coordinates for texture mapping but initialized to (-100000, -100000) for shader use. If loaded model doesnt update it engine knows it is not textured
         glm::vec2 uv = glm::vec2(-100000.0f);
+        uint32_t textureIndex = 0;
     };
 
-    /// @brief Submesh structure for mesh data, its made like this cause some file formats hold multiple meshes in one file, but engine supports only one mesh per file
-    struct Submesh {
-        std::vector<Vertex> vertices;
-        std::vector<uint32_t> indices;
-        std::string texturePath;
-        std::vector<glm::vec3> triangleCenters;
+    /// @brief Submesh range structure for mesh data
+    struct SubmeshRange {
+        uint32_t firstIndex;
+        uint32_t indexCount;
+        uint32_t textureIndex;
     };
 
     /// @brief Mesh data structure for loading and managing mesh data
     struct MeshData {
-        std::vector<Submesh> submeshes;
+        std::vector<Vertex> vertices;
+        std::vector<uint32_t> indices;
+        //std::string texturePath;
+        std::vector<std::string> texturePaths;
+        std::vector<SubmeshRange> submeshes;
         std::string meshPath;
 
+<<<<<<< Updated upstream
         /// @brief Loads mesh using VirtualFileSystem (supports compressed VPKs)
+=======
+        /// @brief Clears all mesh data
+        void clear(){
+            vertices.clear();
+            indices.clear();
+            texturePaths.clear();
+            submeshes.clear();
+            meshPath.clear();
+        }
+
+        /// @brief Loads mesh data from a file using the Virtual File System.
+        /// @details Uses Assimp with a custom `VPKAssimpIOSystem` to read files directly from VFS memory buffers (supporting compressed VPKs).
+        /// Post-processes the mesh to Triangulate, Generate Normals, and Flip UVs.
+        /// @param const std::string& path - Path to the mesh file within the VFS.
+        /// @param VirtualFileSystem* vfs - Pointer to the active VirtualFileSystem instance.
+>>>>>>> Stashed changes
         void loadFromFile(const std::string& path, VirtualFileSystem* vfs);
 
         /// @brief Loads mesh directly from disk relative to executable (Skips VFS, useful for Editor assets)
         void loadFromRawFile(const std::string& relativePath);
-
-        void clear() {
-            submeshes.clear();
-        }
 
     private:
         /// @brief internal helper to convert Assimp scene to Submeshes
